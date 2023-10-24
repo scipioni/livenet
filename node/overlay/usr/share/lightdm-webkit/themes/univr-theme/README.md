9bf22926ea624bd8430962c20b6c6ce7961e7dac
Igor Rossetti <igor.rossetti@csgalileo.net>
2023-10-24 15:18:05 +0200

    Aggiunto pacchetti per pam

--- node/overlay/usr/share/lightdm-webkit/themes/univr-theme/README.md
+++
@@ -1,3 +0,0 @@
-lightdm-webkit-theme
-
-Thanks to https://github.com/omgmog/lightdm-webkit-google
--- node/overlay/usr/share/lightdm-webkit/themes/univr-theme/js/env.sh
+++
@@ -1,7 +0,0 @@
-DEVNAME=$(ip address | grep 'state UP' | awk '{print $2}' | sed s/://g)
-IPNAME=$(ip address show dev $DEVNAME | grep inet | awk '{print $2}' | cut -f 1 -d /)
-
-echo "env = {"
-echo "  PROFILE: '$PROFILE',"
-echo "  IPNAME: '$IPNAME'"
-echo "}"
