622a99d2ebd0e03da67f63575988c2a77cf5d49f
Igor Rossetti <igor.rossetti@csgalileo.net>
2023-10-24 09:52:23 +0200

    Modifiche varie
    - Per prova ho scorporato la lista dei pacchetti di uno stage di docker e messe alcune istruzioni in uno script
      nella directory assets_compose/. Il Dockerfile ha i commenti, quindi si può tornare subito indietro.
    - Aggiunto il tema univr, ma non viene caricato, perciò devo fare il FIX
    - FIX: il layout italiano non veniva preso dal terminale Gnome.
    - Aggiunta la possibilità di avere più layout tastiera nella sessione utente (richiesta del prof. Posenato).

---
+++ node/assets_compose/packages.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+#export DEBIAN_FRONTEND=noninteractive
+export GREETER_VERSION=3.5.2
+
+apt-get update && \
+  xargs -a packages.list sudo DEBIAN_FRONTEND=noninteractive apt install  -qq -y \
+  && apt-get clean
+
+  # apt-get install -qq -y \
+  # ubuntu-desktop \
+  # language-selector-common \
+  # language-pack-it \
+  # language-pack-en \
+  # witalian \
+  # wamerican \
+  # wbritish \
+  # lightdm \
+  # lightdm-gtk-greeter \
+  # plymouth-themes \
+  # plymouth-theme-ubuntu-gnome-logo \
+  # mc tree  nano netcat \
+  # wget curl \
+  # && apt-get clean
+
+echo /usr/sbin/lightdm > /etc/X11/default-display-manager 
+dpkg-reconfigure -fnoninteractive lightdm
+
+cd /tmp && \
+  wget https://github.com/JezerM/web-greeter/releases/download/${GREETER_VERSION}/web-greeter-${GREETER_VERSION}-ubuntu.deb && \
+  apt-get install -qq -y ./web-greeter-${GREETER_VERSION}-ubuntu.deb  
