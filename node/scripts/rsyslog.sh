8ada1f7c6ea59b4efeae73b2946467216a59e07f
Igor Rossetti <igor.rossetti@csgalileo.net>
2023-11-16 16:35:15 +0100

    Effettuate alcune modifiche minori
    Pulizia script
    Deploy rsyslog in container

---
+++ node/scripts/rsyslog.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker rm --force rsyslog
+sudo rm -r /tmp/{conf,log}
+mkdir -p /tmp/{conf,log}
+touch /tmp/log/syslog
+
+cat > /tmp/conf/10_config.conf << EOF
+input(type="imudp" port="5140")
+input(type="imtcp" port="5140")
+
+*.*;auth,authpriv.none          -/var/log/syslog
+EOF
+      
+docker run -d --name rsyslog  --network="host" -v /tmp/conf:/etc/rsyslog.d  -v /tmp/log/syslog:/var/log/syslog  levonet/rsyslog
