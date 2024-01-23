cec5d6e2433628773be986ce75362b8217cc1c70
Igor Rossetti <igor.rossetti@csgalileo.net>
2024-01-23 10:23:29 +0100

    correzioni su script di creazione rsyslog

--- node/overlay/usr/share/web-greeter/themes/univr/js/env.sh
+++ node/overlay/usr/share/web-greeter/themes/univr/js/env.sh
@@ -1,5 +1,5 @@
 DEVNAME=$(ip address | grep 'state UP' | awk '{print $2}' | sed s/://g)
-IPNAME=$(ip address show dev $DEVNAME | grep inet | awk '{print $2}' | cut -f 1 -d /)
+IPNAME=$(ip address show dev $DEVNAME | grep -w inet | awk '{print $2}' | cut -f 1 -d /)
 
 echo "env = {"
 echo "  PROFILE: '$PROFILE',"
