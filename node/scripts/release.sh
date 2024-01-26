78073b5f5ee18c22d92c052da16bc29e212714da
Igor Rossetti <igor.rossetti@csgalileo.net>
2024-01-26 10:03:55 +0100

    sostituito comando docker con parametro per ambienti differenti

--- node/scripts/release.sh
+++ node/scripts/release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+. .env
 
-state=$(docker inspect lnode | jq ".[0] .State .Running")
+state=$( ${_docker} inspect lnode | jq ".[0] .State .Running")
 [ $state = true ] || (echo "container is not running" && exit 1)
 # RELEASE del server in esecuzione
-RELEASE=$(docker-compose exec -T lnode cat /etc/livenet/release | cut -d'=' -f2)
+RELEASE=$( ${_compose} exec -T lnode cat /etc/livenet/release | cut -d'=' -f2)
 [ -n "$RELEASE" ] || exit 1
 logger "current RELEASE: $RELEASE"
 MINOR=$(echo $RELEASE | cut -d'.' -f2 )
