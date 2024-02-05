745c7ccca88e2abf983ebca1d5b4d2b3d985a9d5
Igor Rossetti <igor.rossetti@csgalileo.net>
2024-02-05 17:09:17 +0100

    Visualizza versione nella login page

--- node/overlay/usr/share/web-greeter/themes/univr/js/env.sh
+++ node/overlay/usr/share/web-greeter/themes/univr/js/env.sh
@@ -1,7 +1,8 @@
 DEVNAME=$(ip address | grep 'state UP' | awk '{print $2}' | sed s/://g)
 IPNAME=$(ip address show dev $DEVNAME | grep -w inet | awk '{print $2}' | cut -f 1 -d /)
-
+RELEASE=$( cat /etc/livenet/release | cut -d'=' -f2)
 echo "env = {"
 echo "  PROFILE: '$PROFILE',"
-echo "  IPNAME: '$IPNAME'"
+echo "  IPNAME: '$IPNAME',"
+echo "  VERSION: '$RELEASE'"
 echo "}"
